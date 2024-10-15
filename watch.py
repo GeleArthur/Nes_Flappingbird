7c9bef429a488269f1a644b8dbc3a404df18e1f2
Bram Verhulst <brammieverhulst@gmail.com>
2024-10-15 10:12:18 +0200

    Add Watch script
    :p

---
+++ watch.py
@@ -0,0 +1,45 @@
+import os
+import time
+import subprocess
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+ignored_folders = ['build', '.vscode', '.git']
+
+class FileWachter(FileSystemEventHandler):
+    def on_modified(self, event):
+
+        for folder in ignored_folders:
+            if folder in event.src_path:
+                return
+            
+        if event.src_path.endswith('.s'):
+            if not event.src_path.endswith('compile.bat'):
+                os.system('cls' if os.name == 'nt' else 'clear')
+                print(f'File changed: {event.src_path}. Running compile.bat...')
+                # Run the compile.bat script
+                subprocess.run([bat_script])
+
+if __name__ == "__main__":
+    folder = os.path.dirname(os.path.abspath(__file__))
+    
+    bat_script = os.path.join(folder, "compile.bat")
+    
+    # Check if compile.bat exists
+    if not os.path.isfile(bat_script):
+        print(f"Error: compile.bat not found in {folder}")
+        exit(1)
+    
+    event_handler = FileWachter()
+    observer = Observer()
+    observer.schedule(event_handler, folder, recursive=True) 
+    # Start the observer
+    observer.start()
+    print(f"Watching folder: {folder}")
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
