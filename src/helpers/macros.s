2c592f7b366a3b803028aa20fe989dffd1b8b2ef
GeleArthur <geleminecrafter@gmail.com>
2024-11-17 23:11:33 +0100

    Moving stuff

--- src/helpers/macros.s
+++ src/helpers/macros.s
@@ -42,3 +42,42 @@
     lda #data
     sta PPU_DATA
 .endmacro
+
+
+
+
+.macro set_sprite_x index, xpos
+   lda xpos
+   sta oam + (index * 4) + 3
+.endmacro
+
+.macro set_sprite_x_a index
+   sta oam + (index * 4) + 3
+.endmacro
+
+.macro set_sprite_y index, ypos
+   lda ypos
+   sta oam + (index * 4) + 0
+.endmacro
+
+.macro set_sprite_y_a index
+   sta oam + (index * 4) + 0
+.endmacro
+
+.macro set_sprite_tile index, tile
+   lda tile
+   sta oam + (index * 4) + 1
+.endmacro
+
+.macro set_sprite_tile_a index
+   sta oam + (index * 4) + 1
+.endmacro
+
+.macro set_sprite_attr index, attr
+   lda attr
+   sta oam + (index * 4) + 2
+.endmacro
+
+.macro set_sprite_attr_a index
+   sta oam + (index * 4) + 2
+.endmacro
--- src/helpers/subroutines.s
+++ src/helpers/subroutines.s
@@ -7,6 +7,31 @@
 .proc ClearRam
     lda #0
     tax
-    
+clearRam:
+    sta $0000,x
+    sta $0100,x ; Are we clearing the stack?
+    sta $0200,x
+    sta $0300,x
+    sta $0400,x
+    sta $0500,x
+    sta $0600,x
+    sta $0700,x
+    inx  ; Once x is at 0 we exit
+    bne clearRam
+    rts
+.endproc
+
+; BROKEN DONT USE
+.proc ClearOAM
+    lda #255
+    ldx #0
+clear_oam:
+    sta oam,x
+    inx
+    inx
+    inx
+    inx 
+    bne clear_oam
+
 .endproc
 
