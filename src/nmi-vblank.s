c6f3c9188d831fd3c2b71c087b4a8d54ecdd2510
GeleArthur <geleminecrafter@gmail.com>
2024-11-19 15:16:50 +0100

    Fixed main loop. Add OAM copy

--- src/nmi-vblank.s
+++ src/nmi-vblank.s
@@ -2,9 +2,25 @@
 ; If 1 the cpu is done calulateing the frame and the ppu should render. The cpu should wait until the ppu is done rendering the frame set to 0
 ; If 0 the ppu is done and the cpu should calulate the next frame. the ppu should also wait for the next render until its set to 1
 nmi_ready: .res 1
+WTF: .res 1
+
+
+.segment "OAM"
+; A easy copy of the OAM we copy over every frame. We don't need this if we program smart ORRR naaaa.
+oam: .res 256 
+
 
 
 .segment "CODE"
+
+.macro COPY_OAM
+
+    lda #$00
+    sta OAM_ADDR
+    lda #>oam
+    sta OAM_DMA ; Suppends the 
+    
+.endmacro
 
 .macro WAIT_UNITL_FRAME_HAS_RENDERED
 @waitVBlank:
@@ -17,11 +33,16 @@ nmi_ready: .res 1
     sta nmi_ready 
 .endmacro
 
+
+
+
 .proc nmi
     SAVE_REGISTERS
     lda nmi_ready
-    bne skipRenderingFrame ; If the nmi_ready is 0 we skip as the cpu is not ready rendering the frame
+    beq skipRenderingFrame ; If the nmi_ready is 0 we skip as the cpu is not ready rendering the frame
     
+    COPY_OAM
+
     ; Copy oam
     ; Copy background
 
@@ -35,6 +56,7 @@ skipRenderingFrame:
 .proc irq
     rti
 .endproc
+
 
 
 ; .proc nmi
