d39cc6d360c627922e9a2c45d8306ee18c07bac8
Gilles Durnez <gilles.durnez@hotmail.com>
2024-12-10 09:11:55 +0100

    Scroll fix for the edge from deleted branch (FU ARTHUR XD)

--- src/background.s
+++ src/background.s
@@ -5,6 +5,7 @@ nametable_ptr_1: .res 1
 nametable_ptr_2: .res 1
 
 scroll_pos: .res 1
+nametableIndex: .res 1
 
 .segment "CODE"
 
@@ -38,18 +39,40 @@ LoadAttribute:
 
 LoadAttributeLoop:
   lda pipes_attribute_table, x      ; load data from address (attribute + the value in x)
-  sta PPU_DATA             ; write to PPU
-  inx                   ; X = X + 1
-  cpx #$40              ; Compare X to hex $08, decimal 8 - copying 8 bytes
+  sta PPU_DATA                      ; write to PPU
+  inx                               ; X = X + 1
+  cpx #$40                          ; Compare X to hex $08, decimal 8 - copying 8 bytes
   bne LoadAttributeLoop
   rts
 .endproc
 
 .proc ScrollBackground
   inc scroll_pos
-  lda scroll_pos
+NTSwapCheck:
+  LDA scroll_pos       ; check if the scroll just wrapped from 255 to 0
+  BNE NTSwapCheckDone
+  
+NTSwap:
+  LDA nametableIndex    ; load current nametable number (0 or 1)
+  EOR #$01              ; exclusive OR of bit 0 will flip that bit
+  STA nametableIndex    ; so if nametable was 0, now 1
+                        ;    if nametable was 1, now 0
+NTSwapCheckDone:
+
+
+;This is the PPU clean up section, so rendering the next frame starts properly.
+LDA #CTRL_NMI|CTRL_SPR_1000
+ORA nametableIndex    ; select correct nametable for bit 0
+STA PPU_CTRL
+
+LDA #MASK_SPR | MASK_BG | MASK_SPR_CLIP | MASK_BG_CLIP   ; enable sprites, enable background, no clipping on left side
+STA PPU_MASK
+
+
+; Load the background to the PPU
+  lda scroll_pos      ;HORIZONTAL
   sta PPU_SCROLL
-  lda #0
+  lda #0              ;(NO) VERTICAL
   sta PPU_SCROLL
   rts
 .endproc
