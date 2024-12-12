29f7f1426ef1559b1f2ca7e48ebf38cefb839951
GeleArthur <geleminecrafter@gmail.com>
2024-12-12 16:45:09 +0100

    Badly reading background source

--- src/background.s
+++ src/background.s
@@ -8,8 +8,8 @@ nametable_ptr: .res 2
 scroll_pos: .res 1
 flippingScroll: .res 1
 
-columnLow:  .res 1  ; low byte of new column address
-columnHigh: .res 1  ; high byte of new column address
+ppuWriteLocation:  .res 2  ; low byte of new column address
+; columnHigh: .res 1  ; high byte of new column address
 sourceLow:  .res 1  ; source for column data
 sourceHigh: .res 1
 columnNumber: .res 1  ; which column of level data to draw
@@ -83,8 +83,8 @@ NTSwapCheckDone:
   lda scroll_pos       ; calculate new column address using scroll register
   lsr A
   lsr A
-  lsr A            ; shift right 3 times = divide by 8
-  sta columnLow    ; $00 to $1F, screen is 32 tiles wide
+  lsr A                   ; shift right 3 times = divide by 8
+  sta ppuWriteLocation    ; $00 to $1F, screen is 32 tiles wide
 
   lda flippingScroll     ; calculate new column address using current nametable
   eor #$01          ; invert low bit, A = $00 or $01
@@ -92,7 +92,7 @@ NTSwapCheckDone:
   asl A             ; $00 or $04
   clc
   adc #$20          ; add high byte of nametable base address ($2000)
-  sta columnHigh    ; now address = $20 or $24 for nametable 0 or 1
+  sta ppuWriteLocation+1    ; now address = $20 or $24 for nametable 0 or 1
 
   lda columnNumber  ; column number * 32 = column data offset
   asl A
@@ -121,86 +121,98 @@ DrawColumn:
   
   lda PPU_STATUS             ; read PPU status to reset the high/low latch
 
-  lda columnHigh
+  
+  lda ppuWriteLocation+1
   sta PPU_ADDR             ; write the high byte of column address
-  lda columnLow
+  lda ppuWriteLocation
   sta PPU_ADDR             ; write the low byte of column address
 
   ldx #$1E              ; copy 30 bytes
   ldy #$00
+  clc
 DrawColumnLoop:
   lda (sourceLow), y
   sta PPU_DATA
-  iny
+  lda sourceLow
+  adc #32
+  sta sourceLow
+
+  bcc @overflowSkip
+  inc sourceLow+1
+  dec sourceLow
+@overflowSkip:
+
+
+  ; iny
   dex
   bne DrawColumnLoop
   rts
 .endproc  
   
-.proc DrawAttributeSlide  
-DrawNewAttributes:
-  lda flippingScroll
-  eor #$01          ; invert low bit, A = $00 or $01
-  asl A             ; shift up, A = $00 or $02
-  asl A             ; $00 or $04
-  clc
-  adc #$23          ; add high byte of attribute base address ($23C0)
-  sta columnHigh    ; now address = $23 or $27 for nametable 0 or 1
+; .proc DrawAttributeSlide  
+; DrawNewAttributes:
+;   lda flippingScroll
+;   eor #$01          ; invert low bit, A = $00 or $01
+;   asl A             ; shift up, A = $00 or $02
+;   asl A             ; $00 or $04
+;   clc
+;   adc #$23          ; add high byte of attribute base address ($23C0)
+;   sta columnHigh    ; now address = $23 or $27 for nametable 0 or 1
   
-  lda scroll_pos
-  lsr A
-  lsr A
-  lsr A
-  lsr A
-  lsr A
-  clc
-  adc #$C0
-  sta columnLow     ; attribute base + scroll / 32
+;   lda scroll_pos
+;   lsr A
+;   lsr A
+;   lsr A
+;   lsr A
+;   lsr A
+;   clc
+;   adc #$C0
+;   sta columnLow     ; attribute base + scroll / 32
 
-  lda columnNumber  ; (column number / 4) * 8 = column data offset
-  and #%11111100
-  asl A
-  sta sourceLow
-  lda columnNumber
-  lsr A
-  lsr A
-  lsr A
-  lsr A
-  lsr A
-  lsr A
-  lsr A
-  sta sourceHigh
+;   lda columnNumber  ; (column number / 4) * 8 = column data offset
+;   and #%11111100
+;   asl A
+;   sta sourceLow
+;   lda columnNumber
+;   lsr A
+;   lsr A
+;   lsr A
+;   lsr A
+;   lsr A
+;   lsr A
+;   lsr A
+;   sta sourceHigh
   
-  lda sourceLow       ; column data start + offset = address to load column data from
-  clc 
-  adc #<(attributeTable01)
-  sta sourceLow
-  lda sourceHigh
-  adc #>(attributeTable01)
-  sta sourceHigh
+;   lda sourceLow       ; column data start + offset = address to load column data from
+;   clc 
+;   adc #<(attributeTable01)
+;   sta sourceLow
+;   lda sourceHigh
+;   adc #>(attributeTable01)
+;   sta sourceHigh
 
-  ldy #$00
-  lda $2002             ; read PPU status to reset the high/low latch
-DrawNewAttributesLoop:
-  lda columnHigh
-  sta $2006             ; write the high byte of column address
-  lda columnLow
-  sta $2006             ; write the low byte of column address
-  lda (sourceLow), y    ; copy new attribute byte
-  sta $2007
+;   ldy #$00
+;   lda $2002             ; read PPU status to reset the high/low latch
+; DrawNewAttributesLoop:
+;   lda columnHigh
+;   sta $2006             ; write the high byte of column address
+;   lda columnLow
+;   sta $2006             ; write the low byte of column address
+;   lda (sourceLow), y    ; copy new attribute byte
+;   sta $2007
   
-  iny
-  cpy #$08              ; copy 8 attribute bytes
-  beq DrawNewAttributesLoopDone 
+;   iny
+;   cpy #$08              ; copy 8 attribute bytes
+;   beq DrawNewAttributesLoopDone 
   
-  lda columnLow         ; next attribute byte is at address + 8
-  clc
-  adc #$08
-  sta columnLow
-  jmp DrawNewAttributesLoop
-DrawNewAttributesLoopDone:
-  rts
-.endproc
+;   lda columnLow         ; next attribute byte is at address + 8
+;   clc
+;   adc #$08
+;   sta columnLow
+;   jmp DrawNewAttributesLoop
+; DrawNewAttributesLoopDone:
+;   rts
+; .endproc
 
 
 
