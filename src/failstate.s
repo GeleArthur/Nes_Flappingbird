d0fd0034757607a33b6bc40312a68b1acd5c484b
Gilles Durnez <gilles.durnez@hotmail.com>
2024-12-17 12:25:31 +0100

    Player score being tracked

--- src/failstate.s
+++ src/failstate.s
@@ -1,16 +1,57 @@
 .segment "ZEROPAGE"
 playerDeathStates: .res 1
+Player1Score: .res 1
+Player2Score: .res 1
+Player3Score: .res 1
+Player4Score: .res 1
+
 
 .segment "CODE"
 
 .macro CHECK_DEATH playerBit
+    ; Check if player died already
     lda playerDeathStates
     and #playerBit
     
-    beq :+
+    beq :++++
+    ; Save player State
     lda playerDeathStates
     eor #playerBit
     sta playerDeathStates
+
+    ; Check if finalPlayer
+    and #%0001111
+    bne :++++
+
+    ; Check if player 1 was last player
+    lda #playerBit
+    and #%00000001
+    beq :+
+    inc Player1Score
+    jmp :++++
+    :
+
+    ; Check if player 2 was last player
+    lda #playerBit
+    and #%00000010
+    beq :+
+    inc Player2Score
+    jmp :+++
+    :
+
+    ; Check if player 3 was last player
+    lda #playerBit
+    and #%00000100
+    beq :+
+    inc Player3Score
+    jmp :++
+    :
+
+    ; Check if player 4 was last player
+    lda #playerBit
+    and #%00001000
+    beq :+
+    inc Player4Score
 :
 .endmacro
 
--- src/helpers/subroutines.s
+++ src/helpers/subroutines.s
@@ -5,11 +5,21 @@
 .endproc
 
 .proc ClearRam
+	lda Player1Score
+	pha
+	lda Player2Score
+	pha
+	lda Player3Score
+	pha
+	lda Player4Score
+	pha
+
 	lda #0		; A = 0
     tax		; X = 0
+
 clearRAM:
 	sta $0,x	; clear $0-$ff
-    cpx #$fe	; last 2 bytes of stack?
+    cpx #$f9	; last 2 bytes of stack?
     bcs skipStack	; don't clear it
 	sta $100,x	; clear $100-$1fd
 skipStack:
@@ -21,6 +31,15 @@ skipStack:
 	sta $700,x	; clear $700-$7ff
     inx		; X = X + 1
     bne clearRAM	; loop 256 times
+
+	pla
+	sta Player4Score
+	pla
+	sta Player3Score
+	pla
+	sta Player2Score
+	pla
+	sta Player1Score
     rts
 
 .endproc
